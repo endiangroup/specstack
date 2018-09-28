066fc3de1db0c8914e9d1da3817a28c9098721bc
Adrian Duke <adrian.duke@gmail.com>
2018-09-28 17:08:25 +0100

    Refactor harness tests

--- cmd/harness_test.go
+++ cmd/harness_test.go
@@ -8,26 +8,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type stdInOutErr struct {
+	stdin, stdout, stderr bytes.Buffer
+}
+
 func Test_PersistenPreRunE_PrintsErrorToStderr(t *testing.T) {
 	mockSs := &specstack.MockSpecStack{}
-	var stdin, stdout, stderr bytes.Buffer
-	h := NewCobraHarness(mockSs, &stdin, &stdout, &stderr)
+	h, io := setupHarness(mockSs)
 
 	mockSs.On("IsRepoInitialised").Return(false)
 
 	h.PersistentPreRunE(nil, nil)
 
-	assert.Contains(t, stderr.String(), ErrUninitialisedRepo.Error())
+	assert.Contains(t, io.stderr.String(), ErrUninitialisedRepo.Error())
 }
 
 func Test_PersistenPreRunE_ReturnsErrorIfUninitialisedRepo(t *testing.T) {
 	mockSs := &specstack.MockSpecStack{}
-	var stdin, stdout, stderr bytes.Buffer
-	h := NewCobraHarness(mockSs, &stdin, &stdout, &stderr)
+	h, _ := setupHarness(mockSs)
 
 	mockSs.On("IsRepoInitialised").Return(false)
 
 	err := h.PersistentPreRunE(nil, nil)
 
 	assert.Equal(t, err, NewCliErr(1, ErrUninitialisedRepo))
+}
+
+func setupHarness(mockSs *specstack.MockSpecStack) (*CobraHarness, *stdInOutErr) {
+	io := stdInOutErr{}
+	return NewCobraHarness(mockSs, &io.stdin, &io.stdout, &io.stderr), &io
 }
