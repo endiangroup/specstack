975932cd3021eae78d37477e34211226a6eda689
Adrian Duke <adrian.duke@gmail.com>
2018-10-17 12:56:01 +0100

    Update repository

--- repository/repository.go
+++ repository/repository.go
@@ -2,11 +2,14 @@ package repository
 
 type Writer interface {
 	Init() error
+	ConfigSet(string, string) error
+	ConfigUnset(string) error
 }
 
 type Reader interface {
 	IsInitialised() bool
-	ConfigGetRegex(string) (string, error)
+	ConfigGetAll() (map[string]string, error)
+	ConfigGet(string) (string, error)
 }
 
 type ReadWriter interface {
--- repository/repository_git.go
+++ repository/repository_git.go
@@ -5,18 +5,58 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
-type GitRepo struct {
-	Path string
+func NewGitConfigErr(gitCmdErr *GitCmdErr) error {
+	switch gitCmdErr.ExitCode {
+	case 1:
+		return GitConfigMissingSectionKeyErr{gitCmdErr}
+	}
+
+	return gitCmdErr
 }
 
-func NewGitRepo(path string) *GitRepo {
-	return &GitRepo{Path: path}
+type GitConfigMissingSectionKeyErr struct {
+	*GitCmdErr
+}
+
+func NewGitCmdErr(stderr string, exitCode int, args ...string) *GitCmdErr {
+	return &GitCmdErr{
+		Stderr:   stderr,
+		ExitCode: exitCode,
+		Args:     args,
+	}
+}
+
+type GitCmdErr struct {
+	Stderr   string
+	ExitCode int
+	Args     []string
+}
+
+func (err GitCmdErr) Error() string {
+	if err.Stderr == "" {
+		return fmt.Sprintf("error running git command (exit code: %d): %s", err.ExitCode, strings.Join(err.Args, " "))
+	}
+
+	return err.Stderr
+}
+
+type GitRepo struct {
+	Path            string
+	ConfigNamespace string
+}
+
+func NewGitRepo(path, configNamespace string) *GitRepo {
+	return &GitRepo{
+		Path:            path,
+		ConfigNamespace: configNamespace,
+	}
 }
 
 func (repo *GitRepo) IsInitialised() bool {
-	_, _, err := repo.runGitCommandRaw("rev-parse")
+	_, _, _, err := repo.runGitCommandRaw("rev-parse")
 
 	return err == nil
 }
@@ -26,11 +66,45 @@ func (repo *GitRepo) Init() error {
 	return err
 }
 
-func (repo *GitRepo) ConfigGetRegex(regex string) (string, error) {
-	return repo.runGitCommand("config", "--get-regex", regex)
+func (repo *GitRepo) ConfigGetAll() (map[string]string, error) {
+	result, err := repo.runGitCommand("config", "--null", "--get-regex", "^"+repo.ConfigNamespace+`\.`)
+	if err != nil {
+		return nil, NewGitConfigErr(err.(*GitCmdErr))
+	}
+
+	configMap := map[string]string{}
+	for _, kvPair := range strings.Split(result, "\x00") {
+		kvPair = strings.TrimSpace(kvPair)
+
+		if kvPair == "" {
+			continue
+		}
+		kvParts := strings.SplitN(kvPair, "\n", 2)
+		if len(kvParts) == 1 {
+			configMap[repo.trimConfigNamespace(kvParts[0])] = ""
+		} else {
+			configMap[repo.trimConfigNamespace(kvParts[0])] = kvParts[1]
+		}
+	}
+
+	return configMap, nil
 }
 
-func (repo *GitRepo) runGitCommandRaw(args ...string) (string, string, error) {
+func (repo *GitRepo) ConfigGet(key string) (string, error) {
+	return repo.runGitCommand("config", "--get", repo.prefixConfigNamespace(key))
+}
+
+func (repo *GitRepo) ConfigSet(key, value string) error {
+	_, err := repo.runGitCommand("config", repo.prefixConfigNamespace(key), value)
+	return err
+}
+func (repo *GitRepo) ConfigUnset(key string) error {
+	_, err := repo.runGitCommand("config", "--unset", repo.prefixConfigNamespace(key))
+
+	return err
+}
+
+func (repo *GitRepo) runGitCommandRaw(args ...string) (string, string, int, error) {
 	cmd := exec.Command("git", args...)
 	cmd.Dir = repo.Path
 	var stdout bytes.Buffer
@@ -38,16 +112,33 @@ func (repo *GitRepo) runGitCommandRaw(args ...string) (string, string, error) {
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
-	return strings.TrimSpace(stdout.String()), strings.TrimSpace(stderr.String()), err
+
+	var exitCode int
+	if err != nil {
+		exitCode = 1
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+				exitCode = status.ExitStatus()
+			}
+		}
+	}
+
+	return strings.TrimSpace(stdout.String()), strings.TrimSpace(stderr.String()), exitCode, err
 }
 
 func (repo *GitRepo) runGitCommand(args ...string) (string, error) {
-	stdout, stderr, err := repo.runGitCommandRaw(args...)
+	stdout, stderr, exitCode, err := repo.runGitCommandRaw(args...)
 	if err != nil {
-		if stderr == "" {
-			stderr = "Error running git command: " + strings.Join(args, " ")
-		}
-		err = fmt.Errorf(stderr)
+		return "", NewGitCmdErr(stderr, exitCode, args...)
 	}
+
 	return stdout, err
+}
+
+func (repo *GitRepo) prefixConfigNamespace(key string) string {
+	return repo.ConfigNamespace + "." + key
+}
+
+func (repo *GitRepo) trimConfigNamespace(key string) string {
+	return strings.TrimPrefix(key, repo.ConfigNamespace+".")
 }
