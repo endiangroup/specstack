e18c6ecc82874ee70cf9493c406c7bfa23cdb65d
Adrian Duke <adrian.duke@gmail.com>
2018-10-23 11:27:24 +0100

    Add SetConfiguration to developer

--- personas/developer.go
+++ personas/developer.go
@@ -9,13 +9,17 @@ import (
 type Developer interface {
 	ListConfiguration(context.Context) (map[string]string, error)
 	GetConfiguration(context.Context, string) (string, error)
+	SetConfiguration(context.Context, string, string) error
 }
 
-func NewDeveloper() *developer {
-	return &developer{}
+func NewDeveloper(configStore config.Storer) *developer {
+	return &developer{
+		configStore: configStore,
+	}
 }
 
 type developer struct {
+	configStore config.Storer
 }
 
 func (d *developer) ListConfiguration(ctx context.Context) (map[string]string, error) {
@@ -24,4 +28,15 @@ func (d *developer) ListConfiguration(ctx context.Context) (map[string]string, e
 
 func (d *developer) GetConfiguration(ctx context.Context, name string) (string, error) {
 	return config.Get(config.FromContext(ctx), name)
+}
+
+func (d *developer) SetConfiguration(ctx context.Context, name, value string) error {
+	c := config.FromContext(ctx)
+
+	err := config.Set(c, name, value)
+	if err != nil {
+		return err
+	}
+
+	return config.Store(d.configStore, c)
 }
