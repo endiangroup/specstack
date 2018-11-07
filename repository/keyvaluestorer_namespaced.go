c8d296b29fd64c2a2580b1e7c509f864edb0a2fa
Adrian Duke <adrian.duke@gmail.com>
2018-11-07 16:59:20 +0000

    Unexport type

--- repository/keyvaluestorer_namespaced.go
+++ repository/keyvaluestorer_namespaced.go
@@ -3,18 +3,18 @@ package repository
 import "strings"
 
 func NewNamespacedKeyValueStorer(keyValueStorer KeyValueStorer, namespace string) KeyValueStorer {
-	return &NamespacedKeyValueStorer{
+	return &namespacedKeyValueStorer{
 		keyValueStorer: keyValueStorer,
 		namespace:      namespace,
 	}
 }
 
-type NamespacedKeyValueStorer struct {
+type namespacedKeyValueStorer struct {
 	keyValueStorer KeyValueStorer
 	namespace      string
 }
 
-func (kv *NamespacedKeyValueStorer) All() (map[string]string, error) {
+func (kv *namespacedKeyValueStorer) All() (map[string]string, error) {
 	allKeyValues, err := kv.keyValueStorer.All()
 	if err != nil {
 		return nil, err
@@ -34,22 +34,22 @@ func (kv *NamespacedKeyValueStorer) All() (map[string]string, error) {
 	return trimedKeyValues, nil
 }
 
-func (kv *NamespacedKeyValueStorer) Get(key string) (string, error) {
+func (kv *namespacedKeyValueStorer) Get(key string) (string, error) {
 	return kv.keyValueStorer.Get(kv.prefixNamespace(key))
 }
 
-func (kv *NamespacedKeyValueStorer) Set(key, value string) error {
+func (kv *namespacedKeyValueStorer) Set(key, value string) error {
 	return kv.keyValueStorer.Set(kv.prefixNamespace(key), value)
 }
 
-func (kv *NamespacedKeyValueStorer) Unset(key string) error {
+func (kv *namespacedKeyValueStorer) Unset(key string) error {
 	return kv.keyValueStorer.Unset(kv.prefixNamespace(key))
 }
 
-func (kv *NamespacedKeyValueStorer) prefixNamespace(key string) string {
+func (kv *namespacedKeyValueStorer) prefixNamespace(key string) string {
 	return kv.namespace + "." + key
 }
 
-func (kv *NamespacedKeyValueStorer) trimNamespace(key string) string {
+func (kv *namespacedKeyValueStorer) trimNamespace(key string) string {
 	return strings.TrimPrefix(key, kv.namespace+".")
 }
