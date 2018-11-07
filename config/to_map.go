2231e3cdf57597e83cc92d7ed54bcddefc7b0966
Adrian Duke <adrian.duke@gmail.com>
2018-11-07 12:34:54 +0000

    Export keys from config package

--- config/to_map.go
+++ config/to_map.go
@@ -1,24 +1,24 @@
 package config
 
 func ToMap(c *Config) map[string]string {
-	target := projectToMap(c.Project, keyProject, map[string]string{})
+	target := projectToMap(c.Project, KeyProject, map[string]string{})
 
-	return userToMap(c.User, keyUser, target)
+	return userToMap(c.User, KeyUser, target)
 }
 
 func userToMap(u *User, key prefix, configMap map[string]string) map[string]string {
-	configMap[key.Append(keyUserName)] = u.Name
-	configMap[key.Append(keyUserEmail)] = u.Email
+	configMap[key.Append(KeyUserName)] = u.Name
+	configMap[key.Append(KeyUserEmail)] = u.Email
 
 	return configMap
 }
 
 func projectToMap(p *Project, key prefix, configMap map[string]string) map[string]string {
-	configMap[key.Append(keyProjectName)] = p.Name
-	configMap[key.Append(keyProjectRemote)] = p.Remote
-	configMap[key.Append(keyProjectFeaturesDir)] = p.FeaturesDir
-	configMap[key.Append(keyProjectPushingMode)] = p.PushingMode
-	configMap[key.Append(keyProjectPullingMode)] = p.PullingMode
+	configMap[key.Append(KeyProjectName)] = p.Name
+	configMap[key.Append(KeyProjectRemote)] = p.Remote
+	configMap[key.Append(KeyProjectFeaturesDir)] = p.FeaturesDir
+	configMap[key.Append(KeyProjectPushingMode)] = p.PushingMode
+	configMap[key.Append(KeyProjectPullingMode)] = p.PullingMode
 
 	return configMap
 }
