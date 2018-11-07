b4ad3c96da791e945d1c8e8313db873a9a7cf339
Adrian Duke <adrian.duke@gmail.com>
2018-11-07 12:35:51 +0000

    Add user.name and user.email validations to config on create

--- config/validations.go
+++ config/validations.go
@@ -1,3 +1,26 @@
 package config
 
+import (
+	"github.com/endiangroup/specstack/errors"
+	"github.com/endiangroup/specstack/validations"
+)
+
 type Validation func(*Config) error
+
+func UserNameCannotBeBlank(c *Config) error {
+	fieldName := KeyUser.Append(KeyUserName)
+	if c.User == nil {
+		return &errors.ValidationField{fieldName, "cannot be blank"}
+	}
+
+	return validations.CannotBeBlank(fieldName, c.User.Name)
+}
+
+func UserEmailCannotBeBlank(c *Config) error {
+	fieldEmail := KeyUser.Append(KeyUserEmail)
+	if c.User == nil {
+		return &errors.ValidationField{fieldEmail, "cannot be blank"}
+	}
+
+	return validations.CannotBeBlank(fieldEmail, c.User.Email)
+}
