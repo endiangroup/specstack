17068f0867789a28ffda82d95cf334100c223645
Paul M Fox <iam@yourheropaul.com>
2018-11-08 14:03:28 +0000

    Feedback fixes

--- metadata/entry_reader.go
+++ metadata/entry_reader.go
@@ -5,6 +5,10 @@ import "time"
 // ID represents the address or other ID of the metadata in the repo
 type ID string
 
+func (i ID) String() string {
+	return string(i)
+}
+
 // An EntryReader reads a metadata enty
 type EntryReader interface {
 	Id() ID
