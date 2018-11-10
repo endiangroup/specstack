5b52adc72100a0ed7874a4bba88bbe1b24167a64
Paul M Fox <iam@yourheropaul.com>
2018-11-10 10:40:56 +0000

    Removed older reader paradigm files

--- specification/reader.go
+++
@@ -1,9 +0,0 @@
-package specification
-
-import gherkin "github.com/DATA-DOG/godog/gherkin"
-
-// FIXME: import gherkin "github.com/cucumber/gherkin-go"
-
-type Reader interface {
-	Stories() []gherkin.Feature
-}
